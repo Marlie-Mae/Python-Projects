837af6c8cf50f6503ee7851aeda71bf4a0af288e
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-13 23:42:52 +0800

    Update HandTracker.py

--- HandTracker.py
+++ HandTracker.py
@@ -13,5 +13,29 @@ cTime = 0
 
 white True:
   succss, img = cap.read()
-
   
+  imgRGB = cb2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  result = hands.process(imgRGB)
+
+  if results.multo_hand_landmarks:
+    for handLms in results.multi_hand_landmarks:
+      for id, lm in enumerate(handLms.landmark):
+        h,w,c = img.shape
+        cx, cy = int(lm.x*w), int (lm.y*h)
+        print(id, cx cy)
+        if id == 0
+            cv2.circle(img, (cx, cy), 10, (255,0,255), cv2.FILLED)
+
+      mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS)
+
+
+cTime = time.time()
+fps = 1/(cTime.pTime)
+pTime = cTime
+
+cv2.imshow("Image", img)
+if cb2.waitKey(1) & 0xFF == ord('q'):
+  break
+
+caps.release()
+cv2.destroyAllWindows()
